4af9b756902f6ae5acd2337629060f9f3350f09c
LanaLong <qmexperience@gmail.com>
2020-05-18 13:04:31 +0300

    added validation function

--- src/js/utils/validation.js
+++ src/js/utils/validation.js
@@ -1,2 +1,28 @@
 const searchForm = document.forms.form;
 const searchInput = searchForm.elements.search;
+const searchButton = document.querySelector('.search__button');
+const searchError = document.querySelector('.search__error');
+
+
+function checkInputValidity(input) {
+    if (input.validity.valueMissing || input.validity.tooShort) {
+        searchError.classList.remove('search__error_hide');
+        return false;
+    } else if (input === 0) {
+        searchError.classList.remove('search__error_hide');
+        return false;
+    }
+    else {
+        searchError.classList.add('search__error_hide');
+        return true;
+    }
+};
+
+function handleValidate(event) {
+    event.preventDefault();
+
+    checkInputValidity(event.target);
+    searchButton.disabled = !searchForm.checkValidity();
+}
+
+export { handleValidate };
