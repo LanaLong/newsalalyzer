9fefbb657bfce5c655e1fe61613629dce59081b6
LanaLong <qmexperience@gmail.com>
2020-05-18 16:35:46 +0300

    create CommitCardList

--- src/js/components/CommitCard.js
+++ src/js/components/CommitCard.js
@@ -1,27 +1,32 @@
 class CommitCard {
-    constructor() {
+    constructor(commitDate, ghAvatar, commitTitle, commitEmail, commitText) {
+        this.commitDate = commitDate;
+        this.ghAvatar = ghAvatar;
+        this.commitTitle = commitTitle;
+        this.commitEmail = commitEmail;
+        this.commitText = commitText;
 
     }
 
     createMarkup(data) {
         return `
             <div class="slider__item">
-                <time class="text slider__data" datetime="2019-08-14">${commitDate}</time>
+                <time class="text slider__data" datetime="2019-08-14">${this.commitDate}</time>
                 <div class="slider__info">
-                    <img src="<%=require('${ghAvatar}').default%>" alt="Avatar img"
+                    <img src="<%=require('${this.ghAvatar}').default%>" alt="Avatar img"
                         class="slider__avatar">
                     <div class="slider__info-text">
-                        <h3 class="title slider__title">${commitTitle}</h3>
-                        <p class="text slider__email">${commitEmail}</p>
+                        <h3 class="title slider__title">${this.commitTitle}</h3>
+                        <p class="text slider__email">${this.commitEmail}</p>
                     </div>
                 </div>
-                <p class="text slider__text">${commitText}</p>
+                <p class="text slider__text">${this.commitText}</p>
             </div>
         `
     }
 
-    const sliderContainer = document.querySelector('.slider__swiper-slide');
+    // const sliderContainer = document.querySelector('.slider__swiper-slide');
     // sliderContainer.append(...);
-
-
 }
+
+export { CommitCard };
--- src/js/components/CommitCardList.js
+++ src/js/components/CommitCardList.js
@@ -1,11 +1,26 @@
+import { CommitCard } from './CommitCard';
+
 class CommitCardList {
     constructor() {
 
     }
 
     createCommitCardList(data) {
-        return `
-            
-        `
+        const sliderWrapper = document.querySelector('.slider__swiper-wrapper');
+        // const sliderItem = document.querySelector('.slider__item');
+
+        data.forEach(commit => {
+            let commitCard = new CommitCard(commit.author.date, commit.author.AVATAR_WHERE_IT, commit.author.name, commit.author.email, commit.message);
+
+            sliderWrapper.insertAdjacentHTML('afterbegin', commitCard.createMarkup());
+        });
     }
 }
+
+// const sliderContainer = document.querySelector('.slider__swiper-container');
+// const sliderWrapper = document.querySelector('.slider__swiper-wrapper');
+// const sliderSlide = document.querySelector('.slider__swiper-slide');
+// const sliderItem = document.querySelector('.slider__item');
+// const sliderData = document.querySelector('.slider__data');
+// const sliderInfo = document.querySelector('.slider__info');
+// const sliderText = document.querySelector('.slider__text');
