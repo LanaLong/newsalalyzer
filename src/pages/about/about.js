e68d1de345f9ca975dfc520b4f8abe6fb65d771b
LanaLong <qmexperience@gmail.com>
2020-05-19 23:27:40 +0300

    some fixed styles / gitlog

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,11 +1,10 @@
 import '../../vendor/swiper/swiper.min.css';
-import { QueryBuilder, QueryBuider } from '../../js/utils/QueryBuilder';
+import { QueryBuider } from '../../js/utils/QueryBuilder';
 import { GitHubApi } from '../../js/modules/GitHubApi';
 import { CommitCardList } from '../../js/components/CommitCardList';
 
 import './about.css';
 import Swiper from 'swiper';
-
 
 const swiper = new Swiper('.swiper-container', {
     // Optional parameters
@@ -25,14 +24,13 @@ const swiper = new Swiper('.swiper-container', {
     },
 });
 
-
-const sliderContainer = document.querySelector('.slider__swiper-container');
-const sliderWrapper = document.querySelector('.slider__swiper-wrapper');
-const sliderSlide = document.querySelector('.slider__swiper-slide');
-const sliderItem = document.querySelector('.slider__item');
-const sliderData = document.querySelector('.slider__data');
-const sliderInfo = document.querySelector('.slider__info');
-const sliderText = document.querySelector('.slider__text');
+// const sliderContainer = document.querySelector('.slider__swiper-container');
+// const sliderWrapper = document.querySelector('.slider__swiper-wrapper');
+// const sliderSlide = document.querySelector('.slider__swiper-slide');
+// const sliderItem = document.querySelector('.slider__item');
+// const sliderData = document.querySelector('.slider__data');
+// const sliderInfo = document.querySelector('.slider__info');
+// const sliderText = document.querySelector('.slider__text');
 
 const githubQueryBuilder = new QueryBuider('https://api.github.com/repos/LanaLong/newsalalyzer/commits');
 const githubClient = new GitHubApi(githubQueryBuilder);
@@ -46,23 +44,9 @@ function viewCommits() {
             commitCardList.createCommitCardList(res);
             console.log('Received commits');
             res.forEach(item => {
-                // посмотреть, что находится внутри коммита
-                // console.log(item);
-
-                // запись коммита в html элемент
-                //
-                // let sliderText = document.createElement('div');
-                // sliderText.classList.add('slider__text');
-                // sliderText.textContent = item.commit.message;
-                // потом этот элемент закинуть в slider_item и добавить в контейнер
-                // аналогично с другими элементами
             });
-            // .then(result => {
-            //     if (result.ok) {
-            //         return result.json();
-            //     }
             //     return Promise.reject(`Ошибка: ${result.status}`);
-            // });
+
         })
         .catch(error => {
             console.log(error);
