6eedbe931fdc6003e7740dfcc13ba47b47efed8b
LanaLong <qmexperience@gmail.com>
2020-05-22 01:30:06 +0300

    trying to fix CORE problem with new alias link from YandexPracticum, but Error is: Failed to load resource: the server responded with a status of 401 (Unauthorized)

--- src/js/modules/NewsApi.js
+++ src/js/modules/NewsApi.js
@@ -1,4 +1,5 @@
 // import { NEWS_API_KEY } from '../constants/constants';
+import { PROXY_URL } from '../constants/constants';
 
 class NewsApi {
 
@@ -16,11 +17,22 @@ class NewsApi {
             .setApiKey(apiKey)
             .build();
 
+        // Version 1 with headers ApiKey
         // return fetch(url, {
         //     headers: {
         //         'X-Api-Key': NEWS_API_KEY
         //     }
         // }).then(res => res.json());
+
+        // Version 2 for fix problem with core
+        // return fetch(PROXY_URL + url)
+        //     .then(res => res.json())
+        //     .catch(error => {
+        //         return Promise.reject(`Ошибка: ${error}`);
+        //         // console.log('Ошибка Api сервера');
+        //     })
+
+        // Version 3 with new alias link from YandexPracticum
         return fetch(url)
             .then(res => res.json())
             .catch(error => {
